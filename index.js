231864a43022f1289a83aacfca7db46bc3239132
acappleman <112197155+acappleman@users.noreply.github.com>
2022-11-29 00:11:36 -0500

    added packages, started array and functions

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'Give me a title',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Which license are you using? (MIT, ISC, SOMETHING ELSE)',
+    name: license,
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('README completed!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const data = generateMarkdown(response);
+            writeToFile('README_example.md', data);
+        })
+}
+
+
 
 // Function call to initialize app
 init();
